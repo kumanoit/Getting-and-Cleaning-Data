388c9fc5eac53c4222a5420844cff2e19ec866b6
kumanoit <kumanoit@outlook.com>
2015-06-22 01:00:31 +0530

    Submitting Script

---
+++ CodeBook.md
@@ -0,0 +1,40 @@
+#Code Book
+
+#Dataset
+This project has a script `run_analysis.R` that processes on Samsung's data set.
+The experiments have been carried out with a group of 30 volunteers within an age bracket of 19-48 years. Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) wearing a smartphone (Samsung Galaxy S II) on the waist. Using its embedded accelerometer and gyroscope, we captured 3-axial linear acceleration and 3-axial angular velocity at a constant rate of 50Hz. The experiments have been video-recorded to label the data manually. The obtained dataset has been randomly partitioned into two sets, where 70% of the volunteers was selected for generating the training data and 30% the test data.
+
+####This briefs about the run_analysis.R script
+
+1. This script firstly creates a `data` directory (if it doesn't exist) where it saves all downloaded information, intermediate data and final tidy dataset output file.
+2. If the file that has to be downloaded and processed is present, then it is deleted first just to ensure that it works on fresh data that has not been corrupted/edited.
+3. It downloads data from url path to the data set which is present in zip format.
+4. The downloaded file is then unzipped.
+5. `Train` and `Test` data are read from respective files (present in unzipped directory) and merged into a single data set.
+6. Features are read from features file which are further merged with the data read in previos step so that every column in above data frame represent one feature. (characteristic of tidy data set)
+7. Mean and standard deviation features are filtered out into another data.frame (`meanStdData`)
+8. Train and test `activityLabels` are read from its respective files and then merged into a single activityLabel object. This activity label is then binded with data set (present in column 3). Activity label is an id that tells about the activity for which every observation(row) in data frame has been recorded. There are 6 such activities viz.
+
+  |ID | ACTIVITY_LABEL|
+|---|---------------:|
+|1 | WALKING|
+|2 | WALKING_UPSTAIRS|
+|3 | WALKING_DOWNSTAIRS|
+|4 | SITTING|
+|5 | STANDING|
+|6 | LAYING|
+
+9. Similarly `subject` is binded with data set (present in column 2). Subject is an id ranging from 1 to 30 which represent the person who took observation.
+10. Thereafter in order to keep the data tidy, column names (features) are converted to its lowercase character and any extra character like hyphen/parenthesis is removed. This ensures that data can be fetched by a simple lowercase name. So, there won't be any ambiguity about camelcase feature name and all functions can be applied successfully as unwanted special characters has been removed which might lead to some error since they are mostly associated with some operations.
+11. After that another data set is created from this filtered data which contains average of all features (`mean` and `standard deviation` features) for each activity and each subject. (This doesn't include data like meanFreq)
+12. A tidy data set should contain sensible information. So, `activity name` is added as first column which contains a comprehensive name for which record was collected.
+13. Finally, the data is written as a text file in `uci_har_dataset_output.txt`.
+14. Each column has a feature name that tells about data. Except the first three all 66 columns are predefined features of data that were read from features file itself. Hence, they are self-explanatory. All features un output data set are `numeric` values.
+15. The first three columns are added from other files
+  1. _first column_ : **activityname** : This is a `factor` having the value as present defined in 8th point
+  2. _second column_ : **subject** : This is a `numeric` as defined in 9th point
+  3. _third column_ : **activitylabel** : This is a `numeric` label associated with activity name
+
+#UNITS
+The features selected for this database come from the accelerometer and gyroscope. These time domain signals are measured in **Hertz**
+The first three features are added manually to find average of all feature values for each subject and activity. So, these values are just an marker to do the computations. Hence, no significant units except they are character and numeric ids.
---
+++ run_analysis.R
@@ -0,0 +1,125 @@
+  
+  ## data dir
+  dataDir <- "./data"
+  
+  ## output file name where output data will be saved
+  outputFileName = "./data/uci_har_dataset_output.txt"
+
+  ## if above data dir doesn't exist then create it
+  if (!file.exists(dataDir)) {
+    message("creating data dir")
+    dir.create(dataDir)
+  }
+
+  ## file link to UCR_HAR_Dataset
+  dataFileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  
+  ## destfile location where above data will be saved
+  zippedData <- "./data/uci_har_dataset.zip"
+  
+  ## if above file is already present then delete that file for a fresh analysis
+  ## also delete directory where this zip file was extracted
+  ## also delete output file
+  if (file.exists(zippedData)) {
+    message("deleting file ", zippedData)
+    file.remove(zippedData)
+    if (dir.exists("./data/UCI HAR Dataset/")) {
+      message("deleting zipped output directory ", "UCI HAR Dataset")
+      unlink("./data/UCI HAR Dataset/", recursive=TRUE)
+    }
+    if (file.exists(outputFileName)) {
+      message("Deleting output file")
+      file.remove(outputFileName)
+    }
+  }
+
+  ## download file and save it in above file path
+  message("downloading data")
+  download.file(dataFileUrl, destfile = zippedData, method = "curl")
+  
+  ## this is the path where all files from above downloaded file would be extracted
+  unzippedFilesPath <- "./data"
+  
+  ## unzipping files
+  message("unzipping data")
+  unzip(zippedData, exdir=unzippedFilesPath)
+
+  ## reading training data set
+  trainData <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+  
+  ## reading testing data set
+  testData <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+  
+  ## 1.Merges the training and the test sets to create one data set.
+  data <- rbind(trainData, testData)
+  
+  ## reading features from features.txt file
+  features <- read.table("UCI HAR Dataset/features.txt")[,2]
+  
+  ## renaming columns with its feature values 
+  colnames(data) <- features
+
+  ## mean features
+  meanFeatures <- grep("mean()", features, fixed=TRUE)
+
+  ## standard deviation features
+  stdFeatures <- grep("std()", features, fixed=TRUE)
+  
+  ## 2 Extracts only the measurements on the mean and standard deviation for each measurement.
+  meanStdData <- data[,c(meanFeatures, stdFeatures)]
+  
+  ########################################################################################################
+  #############                           Activity labels file operations                    #############
+  ########################################################################################################
+
+  ## reading training activity label
+  trainActivityLabel <- read.table("./data/UCI HAR Dataset/train/y_train.txt")[, 1]
+  
+  ## reading testing activity label
+  testActivityLabel <- read.table("./data/UCI HAR Dataset/test/y_test.txt")[, 1]
+  
+  ## overall activity label for entire data set
+  activityLabel <- c(trainActivityLabel, testActivityLabel)
+  
+  ## adding activityLabel to data set
+  tidyData <- cbind(activityLabel = activityLabel, meanStdData)
+  
+  ########################################################################################################
+  #############                           Subject file operations                            #############
+  ########################################################################################################
+  
+  ## reading training subject file
+  trainingSubject <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")[, 1]
+  
+  ## reading testing subject file
+  testSubject <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")[, 1]
+  
+  ## overall subject for complete data set
+  subject <- c(trainingSubject, testSubject)
+  
+  ## adding subject info into tidy data set
+  tidyData <- cbind(subject = subject, tidyData)
+  
+  ## 4.Appropriately labels the data set with descriptive variable names.
+  ## changing case to lowercase character
+  colnames(tidyData) <- tolower(colnames(tidyData))
+  ## removing hyphen from feature names
+  colnames(tidyData) <- gsub("-", "", colnames(tidyData))
+  ## removing parenthesis as it is reserved for function. So, having it in feature name might lead to inappropriate behavior
+  colnames(tidyData) <- gsub("\\(\\)", "", colnames(tidyData))
+  
+  ## loading plyr library
+  library(plyr)
+  ## 5. From the data set in step 4, creates a second, independent tidy data 
+  ##   set with the average of each variable for each activity and each subject.
+  tidyDataSet2 <- ddply(tidyData, .(activitylabel, subject), colMeans)
+  
+  ## reading activity labels names
+  activityLabelNames = as.character(read.table("./data/UCI HAR Dataset/activity_labels.txt")[, 2])
+  
+  ## adding activity label names in tidyData set
+  ## 3. Uses descriptive activity names to name the activities in the data set
+  tidyDataSet2 <- cbind(activityname=activityLabelNames[tidyDataSet2$activitylabel], tidyDataSet2)
+  
+  ## saving final data set in output file
+  write.table(tidyDataSet2, outputFileName, row.name=FALSE)
